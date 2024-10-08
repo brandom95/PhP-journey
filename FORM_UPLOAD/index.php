cd6fc310cd571d97016b61d85da315159a9a8d84
brandom95 <brandom.ochoa95@gmail.com>
2024-10-07 22:05:54 -0400

    october 7
    october 7

--- FORM_UPLOAD/index.php
+++ FORM_UPLOAD/index.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>File Uploading</title>
+    <title>File Uploadings </title>
 </head>
 <body>
-    <h1>Upload a File</h1>
+    <h1>Upload a File or many files </h1>
     <form action="upload.php" method="POST" enctype="multipart/form-data">
-        select file: <input type="file" name="file" > <br> <br>
+        select files: <input type="file" name="files[]" multiple> <br> <br>
         <input type="submit" value="Upload File">
     </form>
 </body>
--- FORM_UPLOAD/upload.php
+++ FORM_UPLOAD/upload.php
@@ -1,43 +1,42 @@
 <?php
 
-
-
 if($_SERVER['REQUEST_METHOD']== 'POST'){
 
-    if($_FILES['file']['error'] == 0){
+    
         $uploaddir = 'UPLOADS/';
 
-        $file_name = basename($_FILES["file"]["name"]);
-        $target_file = $uploaddir .$file_name;
+//         if(!is_dir($uploaddir)){
+//             mkdir($uploaddir,0777,true);
+//         }
+// }
 
-        $file_size = $_FILES["file"]["size"];
-        $file_type = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-        $allowed_types = ['jpg', 'png', 'gif', 'jpeg'];
+foreach ($_FILES['files']['name'] as $key => $file_name){
+    $file_tmp = $_FILES['files']['tmp_name'][$key];
+    $file_size = $_FILES['files']['size'][$key];
+    $file_error = $_FILES['files']['error'][$key];
+    $file_type = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+    $target_file = $uploaddir.basename($file_name);
 
-        //checking file size
-        if($file_size > 4 * 1024 * 1024){
-            $file_error= "your file is too large {$file_sieze}";
+    if($file_error === UPLOAD_ERR_OK){
+        if($file_size > 5 * 1024 * 1024){
+            echo "Error too big";
+        }elseif(!in_array($file_type,['jpg','jpeg','png','pdf'])){
+            echo "Error file type $file_name is invalid";
+        }else{
+            if(move_uploaded_file($file_tmp,$target_file)){
+                echo "File {$file_name} uploaded successfully ";
+                
+             //error if statement   
+            }else{
+                echo "Error: there was an issue uploading ";
+            }
         }
-
-        // checking file type
-        else if(!in_array($file_name, $allowed_types)){
-            $file_error = "only JPG, JPG, PNG and GIF are allowed";
-
-            //if no errors, move the file to the directory
-        } else{
-
-
-        if(!move_uploaded_file($_FILES['file']['tmp_name'], $target_file)){
-            $file_error =  'the file'.basename($_FILES["file"]["name"])."has been uploaded";
-        }
-
-            
-        }
-
-       
-        
+        //for loop error check
+    }else{
+        echo "Error: the file {$file_name} failed to upload due to error" ; 
     }
 }
 
 
+}
 ?>

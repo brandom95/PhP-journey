d7c796af7a38b0a1d242c2effe64f85a07132042
brandom95 <brandom.ochoa95@gmail.com>
2024-10-08 22:18:28 -0400

    october 8
    october8

---
+++ LOGIN/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login Page </h2>
+    <form method="POST">
+    <label for="username" >Username</label>
+    <input id="username" type="text" name="username"><br><br>
+    <label for="password" >password</label>
+    <input id="password" type="text" name="password"><br><br>
+
+    <input type="submit" value="Login">
+    
+    </form>
+</body>
+</html>
+
+<?php
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+}
+
+?>
---
+++ SESSION/index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+$_SESSION['username']= "Brandom";
+
+
+?>
+
+<a href="about.php">About</a>
+<a href="contact.php">Contact</a>

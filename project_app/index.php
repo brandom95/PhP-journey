9dfbd1f271d3a8975232e62e78a8e6431c63cdf9
brandom95 <brandom.ochoa95@gmail.com>
2024-10-09 21:39:22 -0400

    october 9
    october 9

---
+++ project_app/index.php
@@ -0,0 +1,17 @@
+<?php
+
+include ''
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login App with SQL and PHP by Jose Brandom Ochoa Nima</title>
+</head>
+<body>
+    <h2>welcome to the homepage of my app </h2>
+</body>
+</html>
